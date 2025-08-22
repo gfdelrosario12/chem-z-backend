8f7263eeb696acf3c6456214c7d7ceee5e3471cd
Gladwin Ferdz Del Rosario <gladelrosario12@gmail.com>
2025-08-23 03:03:00 +0800

    feat: added activity table

---
+++ src/main/java/com/chemz/lms/service/ActivityService.java
@@ -0,0 +1,26 @@
+package com.chemz.lms.service;
+
+import com.chemz.lms.model.Activity;
+import com.chemz.lms.model.Course;
+import com.chemz.lms.repository.ActivityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityService(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    public Activity createActivity(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    public List<Activity> getActivitiesByCourse(Long courseId) {
+        return activityRepository.findByCourseId(courseId);
+    }
+}
