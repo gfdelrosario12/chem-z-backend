1341681a85ba2e7ead77e05904de521a7330fb75
Gladwin Ferdz Del Rosario <gladelrosario12@gmail.com>
2025-09-29 03:50:04 +0800

    fix: connected class feed to teacher role

--- src/main/java/com/chemz/lms/repository/StudentRepository.java
+++ src/main/java/com/chemz/lms/repository/StudentRepository.java
@@ -2,8 +2,12 @@ package com.chemz.lms.repository;
 
 import com.chemz.lms.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;       // <-- add this import
 import java.util.Optional;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Optional<Student> findByUsername(String username);
